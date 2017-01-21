8afc95bcf56636c8de60d010962fbc21c2738e0a
Heidi Rodriguez <hrod2645@gmail.com>
2017-01-21 13:52:19 -0700

    set entry point for routes in index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,21 @@
 //presentation components receive functions and data that they need from a
 //container component, and rely on props to display UI
 
+
+//This is where we update our application's entry point to finalize routing
 /*eslint-disable import/default */
-import 'babel-polyfill';
+import 'babel-polyfill';//this extension or plug-in takes care of any transpiling
+                        //that babel itself cannot handle.
 import React from 'react';
-import { render } from 'react-dom';
+import { render } from 'react-dom'; //pulling react-dom so we have a render function
+                                    //that works in the browser
+import { Router, browserHistory } from 'react-router';//browserHistory takes care
+                                                      //the hash tag on URLs
+import routes from './routes';//referecing the routes.js file
+import './styles/styles.css'; //Webpack can import CSS files, too!
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(
+    <Router history = {browserHistory} routes = {routes} />,
+      document.getElementById('app')
+);
