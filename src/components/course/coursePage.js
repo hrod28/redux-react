28562d483818257df098108ac5fc12727347732a
Heidi Rodriguez <hrod2645@gmail.com>
2017-01-22 05:55:12 -0700

    fixed functionality on form submit

--- src/components/course/coursePage.js
+++ src/components/course/coursePage.js
@@ -2,27 +2,22 @@
 
 import React, {PropTypes} from 'react';
 
-//traditionally, the form would be in a sepatate folder for markup
-
-//first we make a contructor so we can initialize state for the form
-// in the constructor
 class coursePage extends React.Component {
     constructor(props, context){
       super(props, context);
-      //setting local state here, and title is the only property that it
-      //will have
       this.state = {
-        course: { title: null}
+        course: { title: ""}
       };
+
+      this.onTitleChange = this.onTitleChange.bind(this);
+      this.onClickSave = this.onClickSave.bind(this);
     }
 
-    //this function pulls in the event,
     onTitleChange(event) {
-        const course = this.state.course;//then pull that value out of that event
-        course.title = event.target.value;// off of the target, and set that to the title
-        this.setState({course: course});// and update our state by calling 'setState'
-    } // the above function will effectively update the state whenever someone presses
-      // a key when they are focused on the title field.
+        const course = this.state.course;
+        course.title = event.target.value;
+        this.setState({course: course});
+    }
 
     onClickSave(){
       alert(`Saving ${this.state.course.title}`);
@@ -31,20 +26,17 @@ class coursePage extends React.Component {
     render() {
       return(
         <div>
-          <h1> Courses< /h1>
-          <h2> Add Course<h2>
-          <input
-            type="text"
-            onChange={this.onTitleChange}
-            value={ this.state.coures.title} />
-
-          <input
-            type="submit"
-            value="Save"
-            onClick ={ this.onClickSave } />
+          <h1> Courses:</h1>
+          <h2> Add Course: </h2>
+          <input type= "text"
+                 onChange={this.onTitleChange}
+                 value={this.state.course.title} />
+          <input type = "submit"
+                 value = "Save"
+                 onClick = {this.onClickSave} />
         </div>
+      );
     }
-  )
 }
 
 export default coursePage;
