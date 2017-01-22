87d8b54eed748b11d26541279c7947452b7c138b
Heidi Rodriguez <hrod2645@gmail.com>
2017-01-22 08:20:14 -0700

    redux infrastructure and entry point for redux

--- src/index.js
+++ src/index.js
@@ -10,16 +10,36 @@
 import 'babel-polyfill';//this extension or plug-in takes care of any transpiling
                         //that babel itself cannot handle.
 import React from 'react';
-import { render } from 'react-dom'; //pulling react-dom so we have a render function
+import { render } from 'react-dom';//pulling react-dom so we have a render function
                                     //that works in the browser
+//the below code sets the app's entry point for redux
+import configureStore from './store/configureStore';
+//the
+import {Provider} from 'react-redux';
 import { Router, browserHistory } from 'react-router';//browserHistory takes care
                                                       //the hash tag on URLs
 import routes from './routes';//referecing the routes.js file
 import './styles/styles.css'; //Webpack can import CSS files, too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-render(
-    <Router history = {browserHistory} routes = {routes} />,
-      document.getElementById('app')
-  
-);
+// we don't need to put a parameter because we set
+// the using es6 default parameter on the
+// courseReducer page: 'courseReducer(action, state = [])'
+      // const store = configureStore();
+      // render(
+      //   <Provider store={store}>
+      //     <Router history={browserHistory} routes={routes} />
+      //   </Provider>,
+      //   document.getElementById('app')
+      // );
+
+      const store = configureStore();
+      //store.dispatch(loadCourses());
+      //store.dispatch(loadAuthors());
+
+      render(
+        <Provider store={store}>
+          <Router history={browserHistory} routes={routes} />
+        </Provider>,
+        document.getElementById('app')
+      );
