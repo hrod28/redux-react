c85ff96caad0d2d8cd061213bf87b15e3fe29c67
Heidi Rodriguez <hrod2645@gmail.com>
2017-01-22 07:10:01 -0700

    configure store

---
+++ src/store/configureStore.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import {createStore} from 'redux';
+import rootReducer from '../reducers';
+//import reduxImmutableStateInvariant from 'redux-immutable-state-invariant'
+
+export default function configureStore(initialState) {
+  return createStore(
+      rootReducer,
+      initialState
+    );
+
+}
