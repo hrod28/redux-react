6cc49e82aaff41a8d0a127650de29636db179727
Heidi Rodriguez <hrod2645@gmail.com>
2017-01-22 07:13:37 -0700

    add middleware function to store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,14 @@
 'use strict';
 
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
-//import reduxImmutableStateInvariant from 'redux-immutable-state-invariant'
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 
 export default function configureStore(initialState) {
   return createStore(
       rootReducer,
-      initialState
+      initialState,
+      applyMiddleware(reduxImmutableStateInvariant())
     );
 
 }
