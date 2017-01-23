25a3a6ae3b4985a6943fdd2f83d8e11ebb79b9c4
Heidi Rodriguez <hrod2645@gmail.com>
2017-01-22 18:06:24 -0700

    create functionality for input typing for manage course form

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -15,6 +15,15 @@ class ManageCoursePage extends React.Component {
       course: Object.assign({}, props.course),
       errors: {}
     };
+
+    this.updateCourseState = this.updateCourseState.bind(this);
+  }
+
+  updateCourseState(event){
+    const field = event.target.name;
+    let course = this.state.course;
+    course[field] = event.target.value;
+    return this.setState({course: course});
   }
 
   render() {
@@ -22,6 +31,7 @@ class ManageCoursePage extends React.Component {
 
         <CourseForm
           allAuthors={this.props.authors}
+          onChange = {this.updateCourseState}
           course={this.state.course}
           errors={this.state.errors}
         />
